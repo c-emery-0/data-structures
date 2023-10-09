c5dd86d2d71016a4c96a246472c5536aa26fc1a5
celeste emery <beecroncher@gmail.com>
2023-10-09 13:50:14 -0500

    update partner programs

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -14,10 +14,56 @@ public class SentenceReverser
     */
     public static String reverse(String sentence)
     {
+        String sentence1 = "";
     	Scanner scanner = new Scanner(sentence);
-    	
-        // Complete this method. Use a Stack.
-        ...
+    	int size  = 0;
+        Stack<String> words = new Stack<>();
+        while (scanner.hasNext()) {
+            size++;
+            String word = scanner.next();
+            
+            words.push(word);
+            if(word.substring(word.length()-1, word.length()).equals(".")){
+                for(int i = 0; i< size; i++){
+                    System.out.println(words);
+                    System.out.println(sentence1);
+                    sentence1 =  sentence1 + words.pop() + " ";
+                }
+                sentence1= sentence1.substring(0, sentence1.length()-1) + ".";
+                size = 0;
+            
+            }
+        }
+
+        
+        int i = 0;
+        int x = 1;
+
+        for( int z = 0; z<2; z++){
+            while(!sentence1.substring(i, x).equals(".")){
+                
+                i++;
+                x++;
+            }
+            System.out.println(i + " ****** " + x);
+            System.out.println(sentence1.substring(x +  1, sentence1.length()-1));
+            sentence1 = sentence1.substring(0, i ) + " "+ sentence1.substring(x +  1, sentence1.length());
+        }
+        
+        /*
+        
+         size = words.size();
+        
+        System.out.println(sentence1);
+
+        for(int i = 0; i< size; i++){
+            
+            System.out.println(words);
+            System.out.println(sentence1);
+            sentence1 = sentence1 + words.pop() + " ";
+        }*/
+
+        return "list: " + sentence1;
 
 
 
